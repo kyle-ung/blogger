d292ef49b8f644b62490a3337cfbf5cb56e26ed2
unknown <kyleung@CALDW0843.amer.gettywan.com>
2013-01-17 13:23:44 -0700

    Tags completed

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ActiveRecord::Base
+
+	attr_accessible :author_name, :body
+
+	belongs_to :article
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '6cf5bb8fedad83a3cfbd328a80b1a88641d4098b7f6fc6c98d7271d211f7610222fe819e2301dd623f5b6b07a4156b67fedc059e253dab9533a934ca7daf9a43'
